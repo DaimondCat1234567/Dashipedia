fa19681a39af3810e1ce789871f20945b658e86f
DiamondCat1234567 <240060202+DaimondCat1234567@users.noreply.github.com>
2026-04-01 12:57:07 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,3 @@
-let content = document.body
-let head = document.head
+//let content = document.body
+//let head = document.head
+document.write("DiamondCat1234567 - создал dash-версию и сделал некоторые изменения в js-версии, еще задеплоил vercel<br>SHAMAN2016 - создал js-версию
