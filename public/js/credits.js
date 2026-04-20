ea04f8b6fc136bff2ca83754e4dbc6c7fa7dea5c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-20 19:54:17 +0300

    Update credits.js

--- public/js/credits.js
+++ public/js/credits.js
@@ -1,3 +1,7 @@
-//let content = document.body
-//let head = document.head
-document.write("DiamondCat1234567 - создал dash-версию и сделал некоторые изменения в js-версии, еще задеплоил vercel<br>SHAMAN2016 - создал js-версию");
+let { body, head } = document
+
+body = `
+  <h1>Dashipedia Credits</h1>
+  <p><a href='https://scratch.mit.edu/users/SHAMAN2016'>SHAMAN2016</a></p>
+  <p><a href='https://scratch.mit.edu/users/diamondcat2026-2'>DiamondCat2026</a></p>
+`
