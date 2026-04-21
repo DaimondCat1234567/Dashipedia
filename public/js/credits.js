48f1ef98a25b8c4fbe13c65348a2dd6585e464dd
DiamondCat1234567 <240060202+DaimondCat1234567@users.noreply.github.com>
2026-04-21 14:29:57 +0300

    Update credits.js

--- public/js/credits.js
+++ public/js/credits.js
@@ -2,6 +2,6 @@ let { body, head } = document
 
 body = `
   <h1>Dashipedia Credits</h1>
-  <p><a href='https://scratch.mit.edu/users/SHAMAN2016'>SHAMAN2016</a></p>
-  <p><a href='https://scratch.mit.edu/users/diamondcat2026-2'>DiamondCat2026</a></p>
+  <p><a href='https://scratch.mit.edu/users/SHAMAN2016'>SHAMAN2016</a> - Dev</p>
+  <p><a href='https://scratch.mit.edu/users/diamondcat2026-2'>DiamondCat2026</a> - Dev 2</p>
 `
